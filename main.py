6d9dd6f91c69c54fc5ceb618c366546d0e2d2b82
SonicDRJ <jhv9096@rit.edu>
2025-09-03 15:18:10 -0400

    Small change to see full JSON
    Made a tweak to see everything in the JSON to better understand what information to be looking for.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 def get_weather():
@@ -12,9 +14,11 @@ def get_weather():
 
     if response.status_code == 200:
         data = response.json()
-        weather = data.get("current_weather", {})
-        print(f"🌤️ Temperature: {weather.get('temperature')}°C")
-        print(f"💨 Wind Speed: {weather.get('windspeed')} km/h")
+        print("Full JSON Response")
+        print(json.dumps(data, indent=4))
+        # weather = data.get("current_weather", {})
+        # print(f"🌤️ Temperature: {weather.get('temperature')}°C")
+        # print(f"💨 Wind Speed: {weather.get('windspeed')} km/h")
     else:
         print(f"❌ Failed to fetch data. Status code: {response.status_code}")
 
